747d34909bcf92c5aa57ad87aa20e6c6f7972753
Ankouri Abdellah <ankouri120@gmai.com>
2022-01-21 22:01:43 +0000

    clean unused code

--- src/components/StoriesLine.jsx
+++ src/components/StoriesLine.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import SingleStory from "./SingleStory";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
